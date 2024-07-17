999d25e9d36a64ffb249bb1e9c3bf4f665ed5652
lava <sama3421x@gmail.com>
2024-07-17 14:26:55 +0300

    e a

--- Program.cs
+++ Program.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine("Hello World!");
+﻿using System.IO;
+using System.Collections.Generic;
+
+var salesFiles = FindFile("stores");
+
+foreach (var file in salesFiles)
+{
+    Console.WriteLine(file);
+}
+
+IEnumerable<string> FindFile(string folderName)
+{
+    List<string> salesFiles = new List<string>();
+
+    var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+    foreach (var file in foundFiles)
+    {
+        // The file name will contain the full path, so only check the end of it
+        if (file.EndsWith("sales.json"))
+        {
+            salesFiles.Add(file);
+        }
+    }
+
+    return salesFiles;
+}
